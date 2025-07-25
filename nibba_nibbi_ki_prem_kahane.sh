1b277ddeeb1c2d151a751be8f06cef8bb3870ce3
SHREYASPATIL2005 <159605502+SHREYASPATIL2005@users.noreply.github.com>
2025-07-25 23:27:37 +0530

    Create  nibba_nibbi_ki_prem_kahane.sh

---
+++ nibba_nibbi_ki_prem_kahane.sh
@@ -0,0 +1,22 @@
+ubuntu@ip-172-31-7-232:~$ cat nibba_nibbi_ki_prem_kahane.sh
+#!/bin/bash
+#Ye hai hamare Nibba Nibbi ki prem kahani
+
+echo " Nibba: Kaisi ho :)"
+
+echo "Nibbi:  Chata Khayega"
+
+echo "Nibba: Jo tum khilao do"
+
+echo "Nibbi: Jara date dekhna aaj ki"
+
+date
+
+echo "Nibba: Mai kabse intezaar karra hu"
+
+uptime
+
+echo "Nibbi: File konse hai tumbhare pass"
+
+ls /home/ubuntu
+
